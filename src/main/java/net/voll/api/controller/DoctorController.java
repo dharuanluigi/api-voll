0975e389b89aa444653c5e606e32ccdbd9a77531
Dharuan Luigi <dharuanluigi@gmail.com>
2022-12-12 20:20:44 -0300

    Adjust dto entity to get named prop and add notation for transactional service from controller

--- src/main/java/net/voll/api/controller/DoctorController.java
+++ src/main/java/net/voll/api/controller/DoctorController.java
@@ -1,11 +1,11 @@
 package net.voll.api.controller;
 
-import net.voll.api.entity.Address;
 import net.voll.api.entity.doctor.Doctor;
 import net.voll.api.entity.doctor.DoctorDTO;
 import net.voll.api.repository.DoctorRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +19,7 @@ public class DoctorController {
     private DoctorRepository repository;
 
     @PostMapping
+    @Transactional
     public void register(@RequestBody DoctorDTO doctorDTO) {
         repository.save(new Doctor(doctorDTO));
     }
--- src/main/java/net/voll/api/entity/doctor/DoctorDTO.java
+++ src/main/java/net/voll/api/entity/doctor/DoctorDTO.java
@@ -1,7 +1,8 @@
 package net.voll.api.entity.doctor;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import net.voll.api.entity.AddressDTO;
 import net.voll.api.entity.doctor.enums.Speciality;
 
-public record DoctorDTO(String name, String email, String crm, Speciality speciality, AddressDTO addressDTO) {
+public record DoctorDTO(String name, String email, String crm, Speciality speciality, @JsonProperty("address") AddressDTO addressDTO) {
 }
