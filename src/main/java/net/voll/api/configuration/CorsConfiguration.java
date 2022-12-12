e06724f6e30dc29994be31363355880d36eabf9b
Dharuan Luigi <dharuanluigi@gmail.com>
2022-12-12 19:38:34 -0300

    Added cors configuration

---
+++ src/main/java/net/voll/api/configuration/CorsConfiguration.java
@@ -0,0 +1,17 @@
+package net.voll.api.configuration;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configurable
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("Get");
+    }
+}
